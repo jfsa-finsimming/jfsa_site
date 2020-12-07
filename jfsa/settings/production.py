84f44484f6c3724c88e8ba5475a61f687c64027d
tomoki11 <swimtomo11@gmail.com>
2020-12-07 20:40:42 +0900

    heroku

--- jfsa/settings/production.py
+++ jfsa/settings/production.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 django_heroku.settings(locals())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['jfsa.herokuapp.com']
 
