2f202bc8d31385f07fa762aaee0ae2aac8bc26dd
tomoki11 <swimtomo11@gmail.com>
2020-12-07 12:50:41 +0900

    heroku

--- jfsa/settings/base.py
+++ jfsa/settings/base.py
@@ -62,15 +62,6 @@ TEMPLATES = [
     },
 ]
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
--- jfsa/settings/production.py
+++ jfsa/settings/production.py
@@ -1,14 +1,33 @@
 # settings/local.py
 
 from .base import *
+import dj_database_url
+import django_heroku
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
+django_heroku.settings(locals())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['jfsa.herokuapp.com']
+
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'name',
+        'USER': 'user',
+        'PASSWORD': '',
+        'HOST': 'host',
+        'PORT': '',
+    }
+}
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
