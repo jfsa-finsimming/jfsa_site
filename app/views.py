01f75e654a22666c57416990d105d9627f9f2da8
tomoki11 <swimtomo11@gmail.com>
2021-11-17 17:18:03 +0900

    error display none

--- app/views.py
+++ app/views.py
@@ -3,16 +3,6 @@ from .models import Team, NationalMember, Position, Member, Tag, NewsPost,PostMa
 from django.core.paginator import Paginator
 from django.views import generic
 from . import calendar
-from django.views.decorators.csrf import requires_csrf_token
-from django.http import HttpResponseServerError
-
-
-@requires_csrf_token
-def my_customized_server_error(request, template_name='500.html'):
-    import sys
-    from django.views import debug
-    error_html = debug.technical_500_response(request, *sys.exc_info()).content
-    return HttpResponseServerError(error_html)
 
 
 class CommonTemplateView(calendar.MonthCalendarMixin, generic.TemplateView):
