c35fac39b95a6e7c86e3f6cfedf80675cae92b4e
tomoki11 <swimtomo11@gmail.com>
2022-02-27 11:28:30 +0900

    jfsa news

--- jfsa/urls.py
+++ jfsa/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
+    path('markdownx/', include('markdownx.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
