b56c87ec0a34d9e2ef4cd01cac399b6baa59ea60
tomoki11 <swimtomo11@gmail.com>
2022-02-28 21:58:29 +0900

    migration reset

--- app/admin.py
+++ app/admin.py
@@ -1,28 +1,31 @@
 from django.contrib import admin
 from .models import Team, NationalMember, Position, Member, Tag, NewsPost, Event, Race, JFSACupResult, JFSACupRecord,  JFSACupMedia
 from .forms import JFSACupForm
+from cloudinary.forms import cl_init_js_callbacks 
 
 @admin.register(JFSACupMedia)
 class JFSACupMediaAdmin(admin.ModelAdmin):
     add_form_template = 'admin/post_form.html'
     change_form_template = 'admin/post_form.html'
 
-    # def get_form(self, request, obj=None, **kwargs):
-    #     try:
-    #         instance = kwargs['instance']
-    #         return JFSACupForm(instance=instance)
-    #     except KeyError:
-    #         return JFSACupForm
+    def get_form(self, request, obj=None, **kwargs):
+        try:
+            instance = kwargs['instance']
+            return JFSACupForm(instance=instance)
+        except KeyError:
+            return JFSACupForm
 
     def add_view(self, request, form_url="", extra_context=None):
         extra_context = extra_context or {}
-        extra_context['form'] = JFSACupForm(request.POST,request.FILES)
+        extra_context = dict( backend_form = JFSACupForm())
+        extra_context['form'] = self.get_form(request.POST,request.FILES)
         return super(JFSACupMediaAdmin, self).add_view(request, form_url=form_url, extra_context=extra_context)
 
     def change_view(self, request, object_id, form_url="", extra_context=None):
         extra_context = extra_context or {}
+        extra_context = dict( backend_form = JFSACupForm())
         post = JFSACupMedia.objects.get(id=object_id)
-        extra_context["form"] = JFSACupForm(request.POST,request.FILES)
+        extra_context["form"] = self.get_form(instancce=post)
         return super(JFSACupMediaAdmin, self).change_view(request, object_id, form_url=form_url, extra_context=extra_context)
 #
     def save_model(self, request, obj, form, change):
